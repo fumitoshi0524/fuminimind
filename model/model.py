e724a08ec0d2a0a27ae64d4ecbcbb4f45bbc5e86
fumitoshi0524 <Fufumitoshi@qq.com>
2026-01-20 17:08:30 +0800

    RMSNORM

--- model/model.py
+++ model/model.py
@@ -69,3 +69,20 @@ class fuminimindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+    
+    def forward(self, x):
+        normed_x = x * self._norm(x.float()).type_as(x)
+        return normed_x * self.weight
